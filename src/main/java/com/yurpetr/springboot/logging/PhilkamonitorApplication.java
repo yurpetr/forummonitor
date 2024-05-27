64aaaff6dd5480293600a7d2ae2669403ac9a185
Yurii Petrov <yurpetr@gmail.com>
2024-05-27 17:39:10 +0300

    initial commit

---
+++ src/main/java/com/yurpetr/springboot/logging/PhilkamonitorApplication.java
@@ -0,0 +1,13 @@
+package com.yurpetr.springboot.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilkamonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilkamonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurpetr/springboot/logging/PhilkamonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurpetr.springboot.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilkamonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
